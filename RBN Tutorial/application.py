f1ce2c0be6b547c0f83af6037b1a1eddf24196ca
GrantDAllen <79861930+GrantDAllen@users.noreply.github.com>
2021-03-18 16:46:14 -0400

    Add files via upload

--- RBN Tutorial/application.py
+++ RBN Tutorial/application.py
@@ -33,13 +33,12 @@ application.add_url_rule('/<username>', 'hello', (lambda username:
 
 """
 
-application.run()
+#application.run()
 
-"""
+
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
+    #application.debug = True
     application.run()
-"""
