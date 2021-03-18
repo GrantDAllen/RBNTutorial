1ba4ef8df4551895b57d308f247f3495745c5d28
GrantDAllen <79861930+GrantDAllen@users.noreply.github.com>
2021-03-18 09:28:16 -0400

    Add files via upload

--- RBN Tutorial/application.py
+++ RBN Tutorial/application.py
@@ -1,10 +1,15 @@
 from flask import Flask
-application = Flask(__name__)
-@application.route("/")
 
-
+#@application.route("/")
 #def index():
 #    return "Index Page"
 
 def hello_word():
     return "Hello, World!"
+    
+
+application = Flask(__name__)
+
+application.add_url_rule('/')
+
+application.run()
