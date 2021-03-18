7e0d5a11ee16ef0ab847fdf1ef0685662c7f041d
GrantDAllen <79861930+GrantDAllen@users.noreply.github.com>
2021-03-18 16:12:26 -0400

    Add files via upload

--- RBN Tutorial/application.py
+++ RBN Tutorial/application.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 # print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+def say_hello():
+    return "Hello, World!"
 
 # some bits of text for the page.
+
+"""
 header_text = '''
     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
 instructions = '''
@@ -13,10 +15,13 @@ instructions = '''
     someone specific.</p>\n'''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
+"""
+
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
+"""
 # add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda: header_text +
     say_hello() + instructions + footer_text))
@@ -26,9 +31,15 @@ application.add_url_rule('/', 'index', (lambda: header_text +
 application.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
 
+"""
+
+application.run()
+
+"""
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
     application.run()
+"""
