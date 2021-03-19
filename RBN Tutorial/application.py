141530bdb3c1fefb04bfd7447fa5cea63284ae37
GrantDAllen <79861930+GrantDAllen@users.noreply.github.com>
2021-03-19 09:19:07 -0400

    Add files via upload

--- RBN Tutorial/application.py
+++ RBN Tutorial/application.py
@@ -9,13 +9,15 @@ def say_hello():
 
 header_text = '''
     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+    
+"""
 instructions = '''
     <p><em>Hint</em>: This is a RESTful web service! Append a username
     to the URL (for example: <code>/Thelonious</code>) to say hello to
     someone specific.</p>\n'''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
-
+"""
 
 
 # EB looks for an 'application' callable by default.
@@ -23,13 +25,12 @@ application = Flask(__name__)
 
 
 # add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+application.add_url_rule('/', 'index', say_hello())
 
 # add a rule when the page is accessed with a name appended to the site
 # URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+#application.add_url_rule('/<username>', 'hello', (lambda username:
+#    header_text + say_hello(username) + home_link + footer_text))
 
 
 
